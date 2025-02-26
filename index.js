3da1b0b591c16754d7a15145bd8dce7506f93004
aiswarya-03-cloud <aiswaryaajith04@gmail.com>
2025-02-26 07:19:50 +0530

    ent

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://front-end-latest-sizzlebytes.vercel.app/"],
+    
     methods: ["GET","PUT","POST","DELETE", "OPTIONS"],
     credentials:true,
 }))
